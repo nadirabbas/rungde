c927feff434986df6e35a429995f52f99caefa16
nadir <nadirabbasforwork@gmail.com>
2024-04-27 14:15:44 +0500

    disable user zoom and select

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,10 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, user-scalable=no"
+        />
         <link rel="manifest" href="/build/manifest.webmanifest" />
         <title>Rung de</title>
     </head>
