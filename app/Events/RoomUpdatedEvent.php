03f5545549174eac850b4fb11ecba00315c1025d
nadir <nadirabbasforwork@gmail.com>
2024-05-01 05:18:35 +0500

    events should broadcast now

--- app/Events/RoomUpdatedEvent.php
+++ app/Events/RoomUpdatedEvent.php
@@ -6,10 +6,11 @@ use App\Models\Room;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class RoomUpdatedEvent implements ShouldBroadcast
+class RoomUpdatedEvent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
