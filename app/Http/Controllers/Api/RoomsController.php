ee1ffcae5da83811cd94b9058488b27d6149c580
nadir <nadirabbasforwork@gmail.com>
2024-05-09 06:29:58 +0500

    put in try catch

--- app/Http/Controllers/Api/RoomsController.php
+++ app/Http/Controllers/Api/RoomsController.php
@@ -49,36 +49,40 @@ class RoomsController extends Controller
             collect($request->room_users)->each(fn ($users, $position) => $room->participants()->where('position', $position)->first()?->update($users));
         }
 
-        if ($request->input('ended_at')) {
-            $participants = $room->participants()->get();
+        try {
+            if ($request->input('ended_at')) {
+                $participants = $room->participants()->get();
 
-            foreach ($participants as $participant) {
-                $data = [
-                    'games_played' => $participant->user->games_played + 1,
-                    'sirs' => $participant->sir_count + $participant->user->sirs
-                ];
+                foreach ($participants as $participant) {
+                    $data = [
+                        'games_played' => $participant->user->games_played + 1,
+                        'sirs' => $participant->sir_count + $participant->user->sirs
+                    ];
 
-                $teammatePosition = $participant->position + 2;
-                $teammatePosition = $teammatePosition > 4 ? $teammatePosition - 4 : $teammatePosition;
-                $teammate = $participants->where('position', $teammatePosition)->first();
-                $isWinner = $participant->user_id === $request->input('last_winner_id') || $teammate->user_id === $request->input('last_winner_id');
+                    $teammatePosition = $participant->position + 2;
+                    $teammatePosition = $teammatePosition > 4 ? $teammatePosition - 4 : $teammatePosition;
+                    $teammate = $participants->where('position', $teammatePosition)->first();
+                    $isWinner = $participant->user_id === $request->input('last_winner_id') || $teammate->user_id === $request->input('last_winner_id');
 
-                if ($isWinner) {
-                    $data['games_won'] = $participant->user->games_won + 1;
-                    $ourScore = $participant->sir_count + $teammate->sir_count;
-                    $didWeSelectRung = $room->rung_selector === $participant->position || $room->rung_selector === $teammatePosition;
+                    if ($isWinner) {
+                        $data['games_won'] = $participant->user->games_won + 1;
+                        $ourScore = $participant->sir_count + $teammate->sir_count;
+                        $didWeSelectRung = $room->rung_selector === $participant->position || $room->rung_selector === $teammatePosition;
 
-                    if ($ourScore === 13) {
-                        if ($didWeSelectRung) {
-                            $data['courts'] = $participant->user->courts + 1;
-                        } else {
-                            $data['goon_courts'] = $participant->user->goon_courts + 1;
+                        if ($ourScore === 13) {
+                            if ($didWeSelectRung) {
+                                $data['courts'] = $participant->user->courts + 1;
+                            } else {
+                                $data['goon_courts'] = $participant->user->goon_courts + 1;
+                            }
                         }
                     }
-                }
 
-                $participant->user->update($data);
+                    $participant->user->update($data);
+                }
             }
+        } catch (\Throwable $th) {
+            //throw $th;
         }
 
         event(new RoomUpdatedEvent($room->fresh()->load('participants')));
