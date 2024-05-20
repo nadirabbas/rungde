50eed833156de51c9f80126bb3beef157b0b206f
nadir <nadirabbasforwork@gmail.com>
2024-05-20 13:38:50 +0500

    use reverb vars in use pusher

--- client/composables/usePusher.ts
+++ client/composables/usePusher.ts
@@ -6,6 +6,9 @@ export const usePusher = () => {
 
     return new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
         cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+        wsHost: import.meta.env.VITE_PUSHER_HOST,
+        wsPort: import.meta.env.VITE_PUSHER_PORT,
+        wssPort: import.meta.env.VITE_PUSHER_PORT,
         authEndpoint: "/broadcasting/auth",
         auth: {
             headers: {
--- post_deploy.sh
+++ post_deploy.sh
@@ -6,8 +6,6 @@ php artisan optimize
 # start the application
 
 php artisan queue:work &
-php artisan queue:work &
-php artisan queue:work &
+php artisan reverb:start --host=0.0.0.0 --port=8082 &
 
-
-php-fpm -D &&  nginx -g "daemon off;"
+php-fpm -D && nginx 
