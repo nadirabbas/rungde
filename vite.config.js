09d6bd61042d950423fc7a353d360d37eb6de3aa
nadir <nadirabbasforwork@gmail.com>
2024-05-23 18:08:41 +0500

    pwa changes

--- vite.config.js
+++ vite.config.js
@@ -11,26 +11,42 @@ export default defineConfig({
         }),
         vue(),
         VitePWA({
-            name: 'Rung de',
-            short_name: 'Rung de',
-            orientation: 'landscape',
             scope: '/',
-            id: '/',
-            start_url: '/',
             buildBase: '/',
             outDir: 'public',
-            registerType: 'autoUpdate', manifest: {
+            registerType: 'autoUpdate',
+            manifest: {
+                id: '/',
+                scope: '/',
+                name: 'Rung de',
+                short_name: 'Rung de',
                 theme_color: '#004e92',
                 orientation: 'landscape',
                 start_url: '/',
                 display: 'fullscreen',
                 icons: [
                     {
-                        src: '/images/icon.jpg',
+                        src: '/images/icon.png',
                         sizes: '1000x1000',
-                        type: 'image/jpeg',
+                        type: 'image/png',
                         purpose: 'any'
                     },
+                ],
+                screenshots: [
+                    {
+                        src: '/images/screenshots/game.png',
+                        sizes: '838x375',
+                        type: 'image/png',
+                        form_factor: 'wide',
+                        label: 'Rung.de game session'
+                    },
+                    {
+                        src: '/images/screenshots/game-narrow.png',
+                        sizes: '885x880',
+                        type: 'image/png',
+                        form_factor: 'narrow',
+                        label: 'Rung.de game session'
+                    }
                 ]
             }
         })
